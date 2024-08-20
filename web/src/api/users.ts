4db06e0638a792bde3a0b05c4e88163e6e4bfa71
rafia9005 <rafia9005@gmail.com>
2024-08-20 20:08:26 +0700

    add: posts detail menu & add: comment input

--- web/src/api/users.ts
+++ web/src/api/users.ts
@@ -1,18 +1,17 @@
-import { JWT_TOKEN } from '../lib/token';
 import { API_URL } from '../config/config';
+import { authCheck } from '$lib/auth';
 
-export const getProfile = async () =>  {
-  authCheck(JWT_TOKEN);
+export const getProfile = async () => {
+  authCheck();
   try {
     const result = await fetch(`${API_URL}/users`, {
       method: 'GET',
       headers: {
-        Authorization: `Bearer ${JWT_TOKEN}`,
+        Authorization: `Bearer `,
       },
     });
-   return result.json();
+    return result.json();
   } catch (error) {
     return error;
   }
-}
-
+};
